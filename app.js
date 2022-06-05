460d12f5a8837871e213e6ee43e96df8df7db2c5
Himanshu <himanshu_196018@saitm.org>
2022-06-05 23:09:13 +0530

    added work on compose contact and about paages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("")
+const ejs = require("ejs");
+const { render } = require("express/lib/response");
 
 const homeStartingContent = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsumhas been the 
 industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type andscrambled it to make a type 
@@ -13,6 +14,8 @@ specimen book.It has survived not only five centuries, but also the leap into el
 const contactContent = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsumhas been the 
 industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type andscrambled it to make a type specimen book.`;
 
+let posts = [];
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended : true }));
@@ -20,7 +23,42 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res) {
+    res.render("home", 
+    {
+        homeText : homeStartingContent,
+        posts: posts
+    });
+    
+})
 
+app.get('/about', function(req, res) {
+    res.render("about", {aboutText : aboutContent});
+})
+
+app.get('/contact', function(req, res) {
+    res.render("contact", {contactText : contactContent});
+})
+
+app.get('/compose', function(req, res) {
+    res.render("compose");
+})
+app.post('/compose', function(req, res) {
+    const post = {
+        title : req.body.titleBody,
+        content : req.body.postBody
+    };
+    posts.push(post);
+    res.redirect('/')
+})
+
+app.get('/posts/:day', function(req, res) {
+    posts.forEach((ele) => {
+        if(ele.title === req.params.day){
+            console.log("Match Found");
+        }
+    });
+})
 
 app.listen("3000", function() {
     console.log("App is running on server 3000");

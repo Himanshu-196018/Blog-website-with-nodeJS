febe888153077d13e335b9ab9aad0d11bf9b894f
Himanshu <himanshu_196018@saitm.org>
2022-06-27 19:10:28 +0530

    database integration done

--- app.js
+++ app.js
@@ -3,64 +3,103 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const { render } = require("express/lib/response");
+const mongoose = require("mongoose");
+const { result } = require("lodash");
 
-const homeStartingContent = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsumhas been the 
-industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type andscrambled it to make a type 
-specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`;
+// constant which will display on the website.
+const homeStartingContent = `Welcome! viewers, I am Himanshu and you are surfing on Himanshu's Blog Website. You can
+read my Blogs here. I try to post a daily blog which is fun to read and contains a lot more knowledge. The blogs i 
+Write are "How to" blogs in which i explain step to complete a particular task and also "Review" blogs in which i give
+my review on various products, applications and sometimes on games also.`;
 
-const aboutContent = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsumhas been the 
-industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type andscrambled it to make a type 
-specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`;
+const aboutContent = `Hello viewers, I am Himanshu. I am an engineering student pursuing the carrer of MERN full stack
+developer. I am a hard worker and goal seeking person. If I set a goal, I do my Best to achieve it. I love to play 
+PC and mobile games. My favourite game if Mobile Legends: Bang Bang. I love Sports too, my favourite sport is Table Tennis`;
 
-const contactContent = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsumhas been the 
-industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type andscrambled it to make a type specimen book.`;
-
-let posts = [];
+const contactContent = [
+    "You can contact me from the following info",
+    "Mobile No.: xxxxxxxxxx",
+    "Email Address:",
+    "1. himanshulohchab111@gmail.com",
+    "2. himanshu_196018@saitm.org",
+];
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    res.render("home", 
-    {
-        homeText : homeStartingContent,
-        posts: posts
-    });
-    
-})
+// connect mongoose database
+mongoose.connect("mongodb://localhost:27017/Blogs", { useNewUrlParser: true });
 
-app.get('/about', function(req, res) {
-    res.render("about", {aboutText : aboutContent});
-})
+//create mongoose schema
+const postSchema = new mongoose.Schema({
+    Title: String,
+    postBody: String,
+});
 
-app.get('/contact', function(req, res) {
-    res.render("contact", {contactText : contactContent});
-})
+//mongoose model
+const Post = new mongoose.model("post", postSchema);
 
-app.get('/compose', function(req, res) {
-    res.render("compose");
-})
-app.post('/compose', function(req, res) {
-    const post = {
-        title : req.body.titleBody,
-        content : req.body.postBody
-    };
-    posts.push(post);
-    res.redirect('/')
-})
+const post1 = new Post({
+    Title: "My First Blog",
+    postBody:
+        "I have the most amazing spring rolls + peanut dipping sauce to share with you today. Perfect dish for summer entertaining!I love this recipe because it requires no real cooking at all (just assembly), looks amazing, and can be adapted to whatever ingredients you have. It’s also so healthy-if you want it to be! I made a summer veggie version with mango, avocado, bell peppers, sprouts, cilantro, lime, peanuts, and green onions. The mango is key here, it adds such great sweetness and compliments the other veggies so well. I’ve been such a fan of mango these days putting it in everything from my favorite kale salad to mango salsa for fish tacos.",
+});
 
-app.get('/posts/:day', function(req, res) {
-    posts.forEach((ele) => {
-        if(_.lowerCase(ele.title) === _.lowerCase(req.params.day)){
-            res.render("post", {postTitle : ele.title, postBody : ele.content})
+//Routes
+
+//Home route
+app.get("/", function (req, res) {
+    Post.find({}, (err, result) => {
+        if (result.length === 0) {
+            post1.save((err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("home", {
+                homeText: homeStartingContent,
+                posts: result,
+            });
         }
     });
-})
+});
 
-app.listen("3000", function() {
+app.get("/about", function (req, res) {
+    res.render("about", { aboutText: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+    res.render("contact", { contactText: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+    res.render("compose");
+});
+app.post("/compose", function (req, res) {
+    const post = {
+        title: req.body.titleBody,
+        content: req.body.postBody,
+    };
+    post.save();
+    res.redirect("/");
+});
+
+app.get("/posts/:title", function (req, res) {
+    Post.find({}, (err, result) => {
+        result.forEach((ele) => {
+            if (_.lowerCase(ele.Title) === _.lowerCase(req.params.title)) {
+                res.render("post", { postTitle: ele.Title, postBody: ele.postBody });
+            }
+        });
+    });
+});
+
+app.listen("3000", function () {
     console.log("App is running on server 3000");
 });
